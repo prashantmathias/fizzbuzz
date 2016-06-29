6a894a58256ab8c20341f7478e12743c34ff2045
prashantmathias <mathiasaudio@gmail.com>
2016-06-29 14:13:02 +0100

    added more tests and completed fizzbuzz program

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,16 @@
-def fizzbuzz(number)
-  "fizz"
+def fizzbuzz(num)
+
+  if num%3 == 0 && num%5 != 0
+    "fizz"
+  elsif num%5 == 0 && num%3 != 0
+    "buzz"
+  elsif (num%3 && num%5) == 0
+    "fizzbuzz"
+  else
+    num
+  end
 
 end
+
+
+#(1..20).each {|x| puts "#{x} --> #{fizzbuzz(x)}" }
