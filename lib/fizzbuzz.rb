bff35cc7dc4ea7f143e580c74cbf90748012823c
prashantmathias <mathiasaudio@gmail.com>
2016-07-04 17:22:59 +0100

    Refactored and repeated code and tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,6 @@
-def fizzbuzz(x)
+def fizzbuzz(n)
    y = ''
-   y << 'fizz' if x % 3 == 0
-   y << 'buzz' if x % 5 == 0
-   y == '' ? x : y
+   y << 'Fizz' if n % 3 == 0
+   y << 'Buzz' if n % 5 == 0
+   y == '' ? n : y
  end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,15 +2,15 @@ require "fizzbuzz"
 
 describe "fizzbuzz" do
   it "returns 'fizz' when passed 24" do
-    expect(fizzbuzz(24)).to eq "fizz"
+    expect(fizzbuzz(24)).to eq "Fizz"
   end
 
  it "returns 'buzz' when passed 25" do
-    expect(fizzbuzz(25)).to eq "buzz"
+    expect(fizzbuzz(25)).to eq "Buzz"
   end
 
   it "returns 'fizzbuzz' when passed 15" do
-    expect(fizzbuzz(15)).to eq "fizzbuzz"
+    expect(fizzbuzz(15)).to eq "FizzBuzz"
   end
 
   it "returns 68 when passed 68" do
@@ -18,10 +18,10 @@ describe "fizzbuzz" do
   end
 
   it "returns 'fizzbuzz' when passed 85" do
-    expect(fizzbuzz(85)).to eq "buzz"
+    expect(fizzbuzz(85)).to eq "Buzz"
   end
 
   it "returns 'fizzbuzz' when passed 95" do
-    expect(fizzbuzz(95)).to eq "buzz"
+    expect(fizzbuzz(95)).to eq "Buzz"
   end
 end
